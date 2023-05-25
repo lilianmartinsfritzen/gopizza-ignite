e78483b176b61083593e68d5c0117d6b83b31538
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2023-05-24 23:41:38 -0300

    feature: added custom fonts

--- App.tsx
+++ App.tsx
@@ -103,7 +103,8 @@ const styles = StyleSheet.create({
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: '600',
+    // fontWeight: '600',
+    fontFamily: 'DMSerifDisplay-Italic',
   },
   sectionDescription: {
     marginTop: 8,
