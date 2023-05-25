78fa12ad05eac4933488a09344c963b8ddfc8b98
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2023-05-24 22:58:21 -0300

    chore: babel plugin module resolver to handle cleaner imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          test: './test',
+          underscore: 'lodash',
+          '@screens': '.src/screens',
+          '@components': '.src/components',
+        },
+      },
+    ],
+  ],
 };
